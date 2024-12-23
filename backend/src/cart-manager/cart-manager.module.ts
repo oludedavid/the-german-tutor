4e930b1e10330cdee00e1eecdfccecc01141ca7b
David Olude <davidolude@Davids-Laptop.fritz.box>
2024-12-23 04:13:51 +0100

    added the client folder and deleted the frontend folder

--- backend/src/cart-manager/cart-manager.module.ts
+++ backend/src/cart-manager/cart-manager.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CartManagerService } from './cart-manager.service';
 import { CartManagerController } from './cart-manager.controller';
 import { CartSchema } from './schemas/cart.schema';
+import { AuthMiddleware } from 'src/authentication-manager/middleware/auth.middleware';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Cart', schema: CartSchema }])],
   providers: [CartManagerService],
   controllers: [CartManagerController],
 })
-export class CartManagerModule {}
+export class CartManagerModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('');
+  }
+}
