a55eb290d170170d9047535762044d3b9312be7d
Olude David <odavidolumide@gmail.com>
2025-01-13 02:35:42 +0100

    completed the refactoring by having a state store manage the log in status of users

--- client/src/components/custom-components/logout.tsx
+++ client/src/components/custom-components/logout.tsx
@@ -3,14 +3,17 @@
 import { Button } from "../ui/button";
 import { useToast } from "@/hooks/use-toast";
 import Auth from "@/helper/fetchData/auth";
+import { useCartStore } from "@/app/store/_store/useCartStore";
+import usePersistStore from "@/helper/usePersistStore";
 
 export default function Logout() {
   const { toast } = useToast();
+  const store = usePersistStore(useCartStore, (state) => state);
 
   const handleLogout = async () => {
     const auth = new Auth();
     auth.logout();
-
+    store?.setLoginStatus(false);
     toast({
       title: "Logout Successful",
       description: "You have been logged out successfully.",
